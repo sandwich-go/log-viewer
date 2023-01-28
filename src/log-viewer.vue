aca6256663091396dffa5a941008b99d8330eecd
hui.wang <hui.wang@centurygame.com>
2023-01-28 12:50:36 +0800

    refactor: white-space

--- src/log-viewer.vue
+++ src/log-viewer.vue
@@ -208,6 +208,9 @@ export default {
 // global style
 .log-viewer-soft-wrap .log-viewer-content-text {
   text-align: justify;
-  white-space: pre-wrap;
+  white-space: -moz-pre-wrap; /* Firefox */
+  white-space: -o-pre-wrap; /* newer Opera */
+  white-space: pre-wrap; /* Chrome; W3C standard */
+  word-wrap: break-word; /* IE */
 }
 </style>
